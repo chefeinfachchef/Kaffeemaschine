4ee5f7f1ffe1a186c269f194b230090d590309b4
laurenz fischbacher <laurenz.fischbacher@bulme.at>
2022-05-12 10:27:18 +0200

    add folder

---
+++ UC_IoTProject_GSFL/Kaffeemachine/cloud.py
@@ -0,0 +1,5 @@
+import time
+from azure.iot.device import IoTHubDeviceClient
+
+RECEIVED_MESSAGES = 0
+CONNECTION_STRING = "HostName=iot3bhwii22-fi.azure-devices.net;DeviceId=Kaffeemaschine;SharedAccessKey=H4q5C+TfTjyUPLiRe8RmbN5UVOjP7zg9gQYFL9Xkyys="
